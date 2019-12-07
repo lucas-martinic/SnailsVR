0da2b516050f4e073b39e16e5bd9efc2a9d08a4b
Sebastian Morr <sebastian@morr.cc>
2019-12-07 20:46:46 +0100

    Add menu music

--- Assets/Scripts/EndOfText.cs
+++ Assets/Scripts/EndOfText.cs
@@ -8,17 +8,4 @@ public class EndOfText : MonoBehaviour
     void OnTriggerEnter(Collider collider) {
         SceneManager.LoadScene("Natasha");
     }
-<<<<<<< HEAD
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
-
-	void OnTriggerEnter(Collider collider) 	{				   
-	SceneManager.LoadScene(1);
-	}
-=======
->>>>>>> 8b2f7be3c4fb3e8a0b21f378217842e921a7f4da
 }
---
+++ Assets/Scripts/Skipper.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Skipper : MonoBehaviour
+{
+    public string nextScene;
+
+    void Update()
+    {
+        if (Input.GetKey("space")) {
+            Debug.Log("spaaaaaace");
+            SceneManager.LoadScene(nextScene);
+        }
+    }
+}
