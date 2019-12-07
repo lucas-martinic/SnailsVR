14ba61d49c7053ddbee057c10d0b056ba707ed5e
lucas-martinic <lucas.martinic@gmail.com>
2019-12-07 16:39:15 +0100

    Main scene

---
+++ Assets/Scripts/RoadLong.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RoadLong : MonoBehaviour
+{
+    public float roadSpeed = 20;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position += roadSpeed * -Vector3.forward * Time.deltaTime;
+    }
+}
