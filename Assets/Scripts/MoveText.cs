ab12e308025689f9478a26285dfabb6f5d017799
Sebastian Morr <sebastian@morr.cc>
2019-12-07 16:53:56 +0100

    Add Story scene and (empty) Natasha scene

---
+++ Assets/Scripts/MoveText.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveText : MonoBehaviour
+{
+    public float speed = 1f;
+
+    void Update()
+    {
+        transform.Translate(0, Time.deltaTime*speed, 0);
+    }
+}
