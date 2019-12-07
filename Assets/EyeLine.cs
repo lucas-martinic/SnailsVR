85e1f5c60c19605a9ad4b2fc65edcec1adeb3420
lucas-martinic <lucas.martinic@gmail.com>
2019-12-07 17:54:42 +0100

    Snail, road, eyes.

---
+++ Assets/EyeLine.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EyeLine : MonoBehaviour
+{
+    public Transform target;
+    LineRenderer lineRenderer;
+    // Start is called before the first frame update
+    void Start()
+    {
+        lineRenderer = GetComponent<LineRenderer>();
+        lineRenderer.SetPosition(0, transform.position);
+    }
+
+    private void Update()
+    {
+        lineRenderer.SetPosition(1, target.transform.position);
+    }
+}
