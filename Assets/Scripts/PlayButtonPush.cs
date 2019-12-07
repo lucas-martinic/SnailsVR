35eec24c0c050ef1115f14d74f5b13ee6bc95a07
lucas-martinic <lucas.martinic@gmail.com>
2019-12-07 20:05:25 +0100

    Commiting again

---
+++ Assets/Scripts/PlayButtonPush.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+public class PlayButtonPush : MonoBehaviour
+{
+    public string scene;
+    void OnTriggerEnter(Collider collider) {
+        SceneManager.LoadScene(scene);
+    }
+}
