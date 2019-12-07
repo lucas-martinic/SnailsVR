952159d7ac6340ff04d31168dca370b4d509cc60
Sebastian Morr <sebastian@morr.cc>
2019-12-07 19:42:44 +0100

    Creat title scene, connect to story

--- Assets/Scripts/PlayButtonPush.cs
+++ Assets/Scripts/PlayButtonPush.cs
@@ -3,9 +3,11 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
+
 public class PlayButtonPush : MonoBehaviour
 {
+    public string scene;
     void OnTriggerEnter(Collider collider) {
-        SceneManager.LoadScene("Main");
+        SceneManager.LoadScene(scene);
     }
 }
